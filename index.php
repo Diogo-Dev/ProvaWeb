33acc761f5b97f128f22a98bfcbae1fd2ff7da33
Diogo <diogomedeirosdealmeida@gmail.com>
2019-06-14 20:40:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<html>
+<head>
+	<title>Livros</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+	<div>
+		<img src="">
+	</div>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+
+  	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+    <div class="navbar-nav">
+      <a class="nav-item nav-link " href="index.php">Home </a>
+      <a class="nav-item nav-link" href="#">Todos os Livros</a>
+      <a class="nav-item nav-link" href="#">Recomendações</a>
+      <a class="nav-item nav-link" href="#">Sobre</a>
+    </div>
+
+  	</div>
+
+	</nav>
+
+	<div>
+		<table border=1 class="striped">
+			<thead>
+				<td>Romance</td>	
+			</thead>
+			<tbody>
+				<td><img src="imagens/livro1.jpg" height="200" width="200"></img></td>
+				<td><img src="imagens/livro2.jpg" height="200" width="200"></img></td>
+				<td><img src="imagens/livro3.jpg" height="200" width="200"></img></td>
+				<td><img src="imagens/livro4.jpg" height="200" width="200"></img></td>
+				<td><img src="imagens/livro5.jpg" height="200" width="200"></img></td>
+				<td><img src="imagens/livro6.jpg" height="200" width="200"></img></td>
+
+			</tbody>
+	</div>
+
+
+
+</body>
+</html>
